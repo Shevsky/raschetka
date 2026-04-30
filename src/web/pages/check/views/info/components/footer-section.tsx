1899f46de2e68fa895ef6a45426e903cd28af6b6
Konstantin Shevsky <shevskiy@tochka.com>
2026-04-30 17:43:57 +0500

    fix: custom modal popup

--- src/web/pages/check/views/info/components/footer-section.tsx
+++ src/web/pages/check/views/info/components/footer-section.tsx
@@ -1,11 +1,10 @@
-import { Button, Group } from '@mantine/core';
-import { miniApp, popup } from '@tma.js/sdk';
+import { Button, Group, Modal, Text } from '@mantine/core';
+import { miniApp } from '@tma.js/sdk';
 import { observer } from 'mobx-react-lite';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CheckStatus } from '~/persistence';
 import { formatMoney } from '~/utils/formatters/format-money';
 import { formatPlural } from '~/utils/formatters/format-plural';
-import { noop } from '~/utils/misc/noop';
 import { CheckContext } from '~/web/pages/check/check.context';
 import { processError } from '~/web/utils/behaviors/process-error';
 import { useAsyncAction } from '~/web/utils/hooks/use-async-action';
@@ -21,36 +20,19 @@ export const FooterSection = mounted(
     const selectableItems = store.selectableItems;
     const selectableItemsSum = store.selectableItemsSum;
 
+    const [isCompleteModalOpen, setCompleteModalOpen] = useState(false);
+
     const [handleComplete, isCompleteLoading] = useAsyncAction(store.handleCompleteCheck, {
       onSuccess: () => miniApp.close(),
       onError: processError
     });
 
     const handleContinue = () => {
-      popup
-        .show({
-          message: 'Точно закрыть чек?'.concat(
-            selectableItems.length && selectableItems.length < check.items!.length
-              ? ` ${formatPlural(selectableItems.length, 'Остался ещё {} не выбранный товар', 'Осталось ещё {} не выбранных товара', 'Осталось ещё {} не выбранных товаров')} на ${formatMoney(selectableItemsSum)}`
-              : ''
-          ),
-          buttons: [
-            {
-              id: 'yes',
-              type: 'destructive',
-              text: 'Да, закрыть'
-            },
-            {
-              type: 'cancel'
-            }
-          ]
-        })
-        .then((result) => {
-          if (result === 'yes') {
-            handleComplete();
-          }
-        })
-        .catch(noop);
+      setCompleteModalOpen(true);
+    };
+
+    const handleCloseCompleteModal = () => {
+      setCompleteModalOpen(false);
     };
 
     if (check.status !== CheckStatus.ACTIVE || !currentUserAuthor) {
@@ -58,11 +40,31 @@ export const FooterSection = mounted(
     }
 
     return (
-      <Group align="center" justify="space-between" wrap="nowrap">
-        <Button size="xs" color="blue" onClick={handleContinue} loading={isCompleteLoading}>
-          Закрыть чек
-        </Button>
-      </Group>
+      <>
+        <Modal opened={isCompleteModalOpen} onClose={handleCloseCompleteModal} centered>
+          <Text>
+            Точно закрыть чек?
+            {selectableItems.length && selectableItems.length < check.items!.length
+              ? ` ${formatPlural(selectableItems.length, 'Остался ещё {} невыбранный товар', 'Осталось ещё {} невыбранных товара', 'Осталось ещё {} невыбранных товаров')} на ${formatMoney(selectableItemsSum)}`
+              : ''}
+          </Text>
+
+          <Group mt="xl" justify="space-between" wrap="nowrap">
+            <Button variant="light" onClick={handleCloseCompleteModal} fullWidth>
+              Отмена
+            </Button>
+            <Button color="red" onClick={handleComplete} loading={isCompleteLoading} fullWidth>
+              Да, закрыть
+            </Button>
+          </Group>
+        </Modal>
+
+        <Group align="center" justify="space-between" wrap="nowrap">
+          <Button size="xs" color="blue" onClick={handleContinue} loading={isCompleteLoading}>
+            Закрыть чек
+          </Button>
+        </Group>
+      </>
     );
   })
 );
