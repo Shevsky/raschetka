80e732da4f8801638b2cddf651602e09faf1601e
Konstantin Shevsky <mail@shevsky.com>
2026-06-13 03:20:13 +0500

    add lobby page to routes

--- src/web/config/routes.config.tsx
+++ src/web/config/routes.config.tsx
@@ -2,6 +2,7 @@ import { RouteObject, useRouteError } from 'react-router-dom';
 import { ErrorLayout } from '~/web/components/layouts/error-layout';
 import { Page } from '~/web/config/pages.config';
 import { CheckPage, loadCheckPage } from '~/web/pages/check';
+import { loadLobbyPage, LobbyPage } from '~/web/pages/lobby';
 import { RootPage } from '~/web/pages/root';
 import { loadUserPage, UserPage } from '~/web/pages/user';
 import { loadUsersPage, UsersPage } from '~/web/pages/users';
@@ -15,7 +16,8 @@ export const routes: Array<RouteObject> = [
     children: [
       { path: Page.USERS, element: <UsersPage />, loader: loadUsersPage, shouldRevalidate, hasErrorBoundary: true },
       { path: Page.USER, element: <UserPage />, loader: loadUserPage, shouldRevalidate },
-      { path: Page.CHECK, element: <CheckPage />, loader: loadCheckPage, shouldRevalidate }
+      { path: Page.CHECK, element: <CheckPage />, loader: loadCheckPage, shouldRevalidate },
+      { path: Page.LOBBY, element: <LobbyPage />, loader: loadLobbyPage, shouldRevalidate }
     ]
   }
 ];
